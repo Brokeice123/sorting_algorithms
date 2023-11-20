9b8c2beb6ccd5b13dd82fba8c5e8a3fc8def402f
Brokeice123 <keithmarkomen@gmail.com>
2023-11-20 19:39:02 +0000

    Insertion Sort Algorithm implementation together with O notation

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order
+ * @list: doubly linked list of integers
+ *
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		tmp = current->next;
+
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+
+			print_list(*list);
+		}
+
+		current = tmp;
+	}
+}
