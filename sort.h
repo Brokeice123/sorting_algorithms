520f6fb651c6970f10e201552e7bb64e73424624
volemy <minayobenevolence0@gmail.com>
2023-11-21 11:09:48 +0000

    Headerfile

--- sort.h
+++ sort.h
@@ -3,7 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,11 +22,14 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
+void temp_swap(int *x, int *y);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-int lomuto_partition(int *array, int left, int right, size_t size);
-void lomuto_quicksort(int *array, int left, int right, size_t size);
-
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 #endif
